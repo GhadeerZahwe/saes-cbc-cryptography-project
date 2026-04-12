c39c0b947dea622382009f4c75ed0a122f7f834c
GhadeerZahwe <GhadeerZahwe>
2026-04-12 18:47:38 +0300

    Added MixColumns

--- src/saes.py
+++ src/saes.py
@@ -3,3 +3,11 @@ def sub_nib(state):
 
 def shift_rows(state):
     return [state[0], state[1], state[3], state[2]]
+
+def mix_columns(state):
+    return [
+        state[0] ^ state[2],
+        state[1] ^ state[3],
+        state[2] ^ state[0],
+        state[3] ^ state[1]
+    ]
