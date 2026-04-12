aa45b3c7b78342e8a37ab25fcc85e393a0a37015
GhadeerZahwe <GhadeerZahwe>
2026-04-12 22:48:46 +0300

    Updated Functions

--- src/cbc.py
+++ src/cbc.py
@@ -1,7 +1,17 @@
 from utils import xor
 from saes import encrypt_block, decrypt_block
 
+def encrypt_cbc(blocks, key, iv):
+    ciphertext = []
+    prev = iv
 
+    for block in blocks:
+        xored = xor(block, prev)
+        encrypted = encrypt_block(xored, key)
+        ciphertext.append(encrypted)
+        prev = encrypted
+
+    return ciphertext
 
 def decrypt_cbc(ciphertext_blocks, key, iv):
     plaintext = []
